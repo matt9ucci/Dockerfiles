c82ad672340f47bac82519c9af8f17c46e46f8a6
Matt Gucci <matt9ucci@gmail.com>
2020-04-04 12:41:11 +0900

    Add Theia

---
+++ Theia/run.ps1
@@ -0,0 +1,8 @@
+param (
+	[ValidateSet('cpp', 'go', 'java', 'python', 'rust', 'swift')]
+	$Language
+)
+
+$image = "theiaide/$($Language ? "theia-${Language}:next" : 'theia')"
+
+docker run -it --init -p 3000:3000 --mount src=$PSScriptRoot,dst=/home/project,type=bind,consistency=cached $image
