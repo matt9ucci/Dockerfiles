8dc660684061e13dec5c9b7d89687e5a61bf5959
Matt Gucci <matt9ucci@gmail.com>
2020-04-05 14:41:35 +0900

    Switch theia to root

--- Theia/run.ps1
+++ Theia/run.ps1
@@ -5,4 +5,4 @@ param (
 
 $image = "theiaide/$($Language ? "theia-${Language}:next" : 'theia')"
 
-docker run -it --init -p 3000:3000 --mount src=$PSScriptRoot,dst=/home/project,type=bind,consistency=cached $image
+docker run -it --init -p 3000:3000 --user root --mount src=$PSScriptRoot,dst=/home/project,type=bind,consistency=cached $image
